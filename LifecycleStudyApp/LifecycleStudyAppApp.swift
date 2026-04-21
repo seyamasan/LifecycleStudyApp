571a26a18934fe9e757575fcf653252858ec7d7f
seyamasan <kst_apr_24@icloud.com>
2026-04-21 20:08:08 +0900

    first commit

---
+++ LifecycleStudyApp/LifecycleStudyAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  LifecycleStudyAppApp.swift
+//  LifecycleStudyApp
+//
+//  Created by 瀬山皐太 on 2026/04/21.
+//
+
+import SwiftUI
+
+@main
+struct LifecycleStudyAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ LifecycleStudyAppTests/LifecycleStudyAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  LifecycleStudyAppTests.swift
+//  LifecycleStudyAppTests
+//
+//  Created by 瀬山皐太 on 2026/04/21.
+//
+
+import Testing
+@testable import LifecycleStudyApp
+
+struct LifecycleStudyAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
